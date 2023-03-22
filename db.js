97cf0f568e929d26895a47a2823d5e17d973f58d
Umerhabib-50 <umr.habib@gmail.com>
2023-03-22 20:58:42 +0500

    created error handler and async wrapper

--- db.js
+++ db.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
 
 const url =
-  "mongodb+srv://umer:1234@cluster0.z6nrdkx.mongodb.net/ecom?retryWrites=true&w=majority";
+  "mongodb+srv://umer:123@cluster0.z6nrdkx.mongodb.net/ecom?retryWrites=true&w=majority";
 
 const connectToDb = async () => {
-  try {
-    await mongoose.connect(url);
-    console.log("database connected");
-  } catch (error) {
-    console.log("error connecting database");
-  }
+  await mongoose.connect(url);
+  console.log("database connected");
 };
 
 module.exports = connectToDb;
--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const connectToDb = require("./db");
 // database connection function
 connectToDb();
 
-app.listen("6000", () => {
+const server = app.listen("6000", () => {
   console.log("server started at 6000 port");
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.message);
+  console.log("shutting down server due to unhandled promise rejection");
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
